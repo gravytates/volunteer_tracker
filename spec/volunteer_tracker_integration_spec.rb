ca13037dc58107be8117825c8f38ff5947b26e1e
Grady L Shelton <gradyish@gmail.com>
2017-05-05 10:41:57 -0700

    pass second integration test showing a project name as updated

--- spec/volunteer_tracker_integration_spec.rb
+++ spec/volunteer_tracker_integration_spec.rb
@@ -1,4 +1,5 @@
 require "capybara/rspec"
+require "spec_helper"
 require "./app"
 
 Capybara.app = Sinatra::Application
@@ -12,5 +13,18 @@ describe('adding a new project', {:type => :feature}) do
 
     click_button('add project')
     expect(page).to have_content('Riparian Restoration')
+  end
+end
+
+describe('updating a project name', {:type => :feature}) do
+  it('allows a user to change a project name') do
+    @project = Project.new({:name => 'Riparian Restoration', :id => nil})
+    @project.save
+    visit("/projects/#{@project.id}")
+
+    fill_in('name', :with =>'Otter Survey')
+
+    click_button('edit')
+    expect(page).to have_content('Otter Survey')
   end
 end
