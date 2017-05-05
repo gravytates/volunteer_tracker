36c3f94ffeeb30c43c8ab9e37b8e0efec0df4180
Grady L Shelton <gradyish@gmail.com>
2017-05-05 10:17:52 -0700

    add sinatra routing and view files to create, read, update and delete projects

--- app.rb
+++ app.rb
@@ -2,10 +2,42 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './lib/project.rb'
 require './lib/volunteer.rb'
+require 'pg'
 require 'pry'
 
 also_reload('lib/**/*.rb')
+DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/projects/new') do
+  name = params.fetch('name')
+  id = nil
+  @project = Project.new(:name => name, :id => nil)
+  @project.save
+  @projects = Project.all
+  erb(:index)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params.fetch("id").to_i)
+  erb(:project)
+end
+
+patch('/projects/:id/edit') do
+  project = Project.find(params.fetch("id").to_i)
+  name = params.fetch('name')
+  project.update({:name => name})
+  @projects = Project.all
+  erb(:index)
+end
+
+delete('/projects/:id/delete') do
+  project = Project.find(params.fetch("id").to_i)
+  project.delete
+  @projects = Project.all
   erb(:index)
 end
